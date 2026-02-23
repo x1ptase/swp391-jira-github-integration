a44179cefff6cfe18b8eef9bf9dbbe5f5e7bd5cc
LocNPD <nguyenphungdailoc@gmail.com>
2026-02-23 13:08:17 +0700

    Thay @Getter @Setter cho gọn

--- backend/src/main/java/com/swp391/backend/dto/response/UserResponse.java
+++ backend/src/main/java/com/swp391/backend/dto/response/UserResponse.java
@@ -1,6 +1,11 @@
 package com.swp391.backend.dto.response;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 public class UserResponse {
     private Long userId;
     private String username;
@@ -14,67 +19,4 @@ public class UserResponse {
     public UserResponse() {
     }
 
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGithubUsername() {
-        return githubUsername;
-    }
-
-    public void setGithubUsername(String githubUsername) {
-        this.githubUsername = githubUsername;
-    }
-
-    public String getJiraAccountId() {
-        return jiraAccountId;
-    }
-
-    public void setJiraAccountId(String jiraAccountId) {
-        this.jiraAccountId = jiraAccountId;
-    }
-
-    public String getRoleCode() {
-        return roleCode;
-    }
-
-    public void setRoleCode(String roleCode) {
-        this.roleCode = roleCode;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
