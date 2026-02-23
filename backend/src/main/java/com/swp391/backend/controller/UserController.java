75b8e7ae0e43d3bb79000c6d0f23e1a376898ebc
LocNPD <nguyenphungdailoc@gmail.com>
2026-02-23 13:12:38 +0700

    Update UserController.java

--- backend/src/main/java/com/swp391/backend/controller/UserController.java
+++ backend/src/main/java/com/swp391/backend/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/users")
-@PreAuthorize("hasAnyRole('ADMIN','LECTURER')")
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
 
     private final UserService userService;
