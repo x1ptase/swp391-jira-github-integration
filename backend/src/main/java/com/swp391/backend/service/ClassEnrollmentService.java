6c71f5f5e273de91f7557d467ccc5c01007b5d60
MinhNT-fptu <ntminh1302@gmail.com>
2026-03-18 08:39:33 +0700

    API: student get enrolled class

--- backend/src/main/java/com/swp391/backend/service/ClassEnrollmentService.java
+++ backend/src/main/java/com/swp391/backend/service/ClassEnrollmentService.java
@@ -1,5 +1,9 @@
 package com.swp391.backend.service;
 
+import com.swp391.backend.dto.response.AcademicClassResponse;
+
 public interface ClassEnrollmentService {
     void enroll(Long classId);
+
+    AcademicClassResponse getMyClass();
 }
