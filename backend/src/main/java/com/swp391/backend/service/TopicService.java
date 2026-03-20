e834e1933e518b4d150d16297169d29a6c0d75e5
Recheal738 <20.huynhminhloc.11a1@gmail.com>
2026-03-20 13:34:17 +0700

    add service

--- backend/src/main/java/com/swp391/backend/service/TopicService.java
+++ backend/src/main/java/com/swp391/backend/service/TopicService.java
@@ -1,4 +1,20 @@
 package com.swp391.backend.service;
 
+import com.swp391.backend.dto.request.CreateTopicRequest;
+import com.swp391.backend.dto.request.UpdateTopicRequest;
+import com.swp391.backend.dto.response.TopicResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface TopicService {
+
+    TopicResponse createTopic(CreateTopicRequest request);
+
+    TopicResponse updateTopic(Long id, UpdateTopicRequest request);
+
+    TopicResponse getTopic(Long id);
+
+    Page<TopicResponse> listTopics(String keyword, Pageable pageable);
+
+    void deleteTopic(Long id);
 }
