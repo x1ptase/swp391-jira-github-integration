427d8db776ff3a973722e6899d51b0392b936568
MinhNT-fptu <ntminh1302@gmail.com>
2026-03-20 17:22:52 +0700

    delete feature student enroll in class

--- backend/src/main/java/com/swp391/backend/service/ClassEnrollmentService.java
+++
@@ -1,9 +0,0 @@
-package com.swp391.backend.service;
-
-import com.swp391.backend.dto.response.AcademicClassResponse;
-
-public interface ClassEnrollmentService {
-    void enroll(Long classId);
-
-    AcademicClassResponse getMyClass();
-}
